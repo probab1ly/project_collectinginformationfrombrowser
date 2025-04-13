45f463d56bde18690df44aa2f82621a9cc2ecd12
Дмитрий <142750962+probab1ly@users.noreply.github.com>
2025-04-13 11:06:09 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 import time
 from fake_useragent import UserAgent
 import json
+import pandas as pd
 spisok = []
 needwords = ['МГЮА', 'Мгюа', 'Кутафина']
 def find(testword):
@@ -16,7 +16,7 @@ def find(testword):
         options.add_argument(f'--user-agent={user_agent}')
         driver = webdriver.Chrome(options=options)
 
-        for i in range(0, 6):
+        for i in range(0, 6):#количество страниц
             driver.get(f'https://ya.ru/search/?text={testword}&search_source=yaru_desktop_common&lr=11031&p={i}')
             print(f'Обрабатывается страница: {i+1}')
             time.sleep(12)
@@ -47,22 +47,39 @@ def find(testword):
         print(f'Произошла ошибка 1: {e}')
     finally:
         driver.quit()
+def save_to_json(w, sp):
+    try:
+        if w == 'Владимир Дмитриевич Никишин':
+            with open('vova.json', 'w', encoding='utf-8') as file1:
+                json.dump(sp, file1, indent=4, ensure_ascii=False)
+        elif w == 'Александр Сергеевич Глазков':
+            with open('aleks.json', 'w', encoding='utf-8') as file2:
+                json.dump(sp, file2, indent=4, ensure_ascii=False)
+        else:
+            with open('test.json', 'w', encoding='utf-8') as file:
+                json.dump(sp, file, indent=4, ensure_ascii=False)
+        print('Данные сохранены в Json')
+    except Exception as e:
+        print(f'Произошла ошибка на этапе сохранения в JSON: {e}')
+def save_to_excel(sp, fil='parserexcel.xlsx'):
+    try:
+        df = pd.DataFrame(sp)
+        writer = pd.ExcelWriter(fil, engine='openpyxl')
+        df.to_excel(writer, index=False, sheet_name='Parse')
+        worksheet = writer.sheets['Parse']
+        worksheet.column_dimensions['A'].width = 140
+        worksheet.column_dimensions['B'].width = 160
+        writer.close()
+        print('Данные сохранены в Excel')
+    except Exception as e:
+        print(f'Произошла ошибка на этапесохранения в Excel: {e}')
 def main():
     print('Введите нужное словосочетание')
     word = input()
     find(word)
-    # spisok1 = [s for s in spisok if s['url'][:23]!='https://yabs.yandex.ru']
     finall_spisok = [dict(s) for s in {frozenset(sp.items()) for sp in spisok}]
-
-    if word == 'Владимир Дмитриевич Никишин':
-        with open('vova.json', 'w', encoding='utf-8') as file1:
-            json.dump(finall_spisok, file1, indent=4, ensure_ascii=False)
-    elif word == 'Александр Сергеевич Глазков':
-        with open('aleks.json', 'w', encoding='utf-8') as file2:
-            json.dump(finall_spisok, file2, indent=4, ensure_ascii=False)
-    else:
-        with open('test.json', 'w', encoding='utf-8') as file:
-            json.dump(finall_spisok, file, indent=4, ensure_ascii=False)
+    save_to_json(word, finall_spisok)
+    save_to_excel(finall_spisok)
 
 if __name__=='__main__':
     main()
